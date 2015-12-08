b4273385a6d7aaf2da52c74f9f340448bfffd393
Philip Sugg <pbensugg@gmail.com>
2015-12-07 18:53:06 -0600

    add basic views to test'

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,10 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
 
+from django.http import HttpRequest
+
+# from django.http import HttpResponse
+
 # Create your tests here.
 
 
@@ -10,3 +14,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+class AnotherTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
