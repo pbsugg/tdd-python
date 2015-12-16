af11bf9cf2c10a345f9dcd19d17c5e06c2015a8c
Philip Sugg <pbensugg@gmail.com>
2015-12-16 10:37:57 -0600

    add refactor to DRY up list element tests

--- function_tests.py
+++ function_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_starts_a_list_and_retrieves_it_later(self):
         # get the homepage
         self.browser.get('http://localhost:8000')
@@ -28,13 +34,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # add first item to the list
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
-        inputbox.send_keys(Keys.RETURN)
-
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy Peacock Feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
         # add second item to the list
@@ -43,12 +45,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # find the item
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: Use peacock feathers to make fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy Peacock Feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
+
 
         # Generate a unique url
 
