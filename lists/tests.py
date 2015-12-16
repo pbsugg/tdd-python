8481912edfe51af115357be88e746bf454c081ca
Philip Sugg <pbensugg@gmail.com>
2015-12-15 18:47:22 -0600

    add suite of passing POST tests, fixed version issue with string rendering

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,12 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
 from django.template.loader import render_to_string
+from django.template import RequestContext
 
 from django.http import HttpRequest
+from django.shortcuts import render_to_response
+
+
 
 # from django.http import HttpResponse
 
@@ -21,5 +25,20 @@ class AnotherTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_saves_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
         self.assertEqual(response.content.decode(), expected_html)
