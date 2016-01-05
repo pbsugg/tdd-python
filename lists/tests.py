b5b671abc4a496c0d0c5bf361289e227fef533d7
Philip Sugg <pbensugg@gmail.com>
2016-01-04 23:09:30 -0600

    add partial solution for new list template partial

--- lists/tests.py
+++ lists/tests.py
@@ -43,7 +43,7 @@ class AnotherTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
 
-    def test_home_page_redirects_after_post(self):
+    def test_home_page_redirects_after_POST(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
@@ -51,7 +51,7 @@ class AnotherTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_saves_all_items(self):
         Item.objects.create(text='itemey 1')
@@ -81,3 +81,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
