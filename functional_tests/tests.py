ff5ef2182aa391b369e8cb570601814f90bb7d40
Philip Sugg <pbensugg@gmail.com>
2016-01-11 12:35:22 -0600

    add completed unit tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
 
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -43,14 +43,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        #
         # get the text box inviting to add a new item
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys( 'Use peacock feathers to make fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        time.sleep(5)
         inputbox.send_keys(Keys.ENTER)
         #
 
-        # # find the item
-        self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
+        # page should update again and show both items
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # quit to wipe out all previous information
